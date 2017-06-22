54959a4714ebefa54d4b909193cc6aebd47a1600
yestamayo <yestamayo@gmail.com>
2017-06-22 15:42:11 -0500

    Pruebas de keyword let

---
+++ scripts/tutorial6.js
@@ -0,0 +1,27 @@
+"use strict";
+function greetPerson(name) {
+    var greet;
+    if (name === "Chandler") {
+        var greet_1 = "Hello Chandler";
+    }
+    else {
+        var greet_2 = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Chandler");
+var a = 1;
+var b = 2;
+if (a === 1) {
+    var a = 10;
+    var b_1 = 20;
+    console.log(a); //10
+    console.log(b_1); //20
+}
+console.log(a); //10
+console.log(b); //2
+var c = 1;
+var c = 2;
+var d = 1;
+//let d=2; 
+//# sourceMappingURL=tutorial6.js.map
